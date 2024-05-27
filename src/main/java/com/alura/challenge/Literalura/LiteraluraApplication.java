e1bbab2929130fda55120eb4cc2ca0d7bb6741cc
Nahuel Pacheco <pacheconahuel101@gmail.com>
2024-05-27 16:55:49 -0300

    Creacion y enlace de DB

--- src/main/java/com/alura/challenge/Literalura/LiteraluraApplication.java
+++ src/main/java/com/alura/challenge/Literalura/LiteraluraApplication.java
@@ -1,13 +1,18 @@
 package com.alura.challenge.Literalura;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiteraluraApplication {
+public class LiteraluraApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hola mundo");
+	}
 }
