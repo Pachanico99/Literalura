d149dfd7f2d56057d33d25a1d8f96d73f94d3e61
Nahuel Pacheco <pacheconahuel101@gmail.com>
2024-05-27 16:44:35 -0300

    Creacion del proyecto

---
+++ src/main/java/com/alura/challenge/Literalura/LiteraluraApplication.java
@@ -0,0 +1,13 @@
+package com.alura.challenge.Literalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraluraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/challenge/Literalura/LiteraluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.challenge.Literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
